901ecc65da6e8baea6318f887e5029485dcdf2ae
Stanislav Huliaiev <101598952+GulShow@users.noreply.github.com>
2023-02-20 19:42:02 +0200

    mvp commit

--- App.tsx
+++ App.tsx
@@ -8,12 +8,15 @@ import MyKeyboard from './src/components/MyKeyboard';
 
 export default function App() {
   const [theme, setTheme] = useState('light');
+  const changeTheme = () => () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
   return (
     <ThemeContext.Provider value={theme}>
       <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: 'black'}]}>
         <Switch
           value={theme === 'dark'}
-          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          onValueChange={changeTheme()}
         />
         <MyKeyboard />
       </SafeAreaView>
